5e0ad564627423ed8a407e46590033a136942037
hudamiftakh <hudamiftakh8@gmail.com>
2025-05-15 21:07:38 +0700

    perbaikan

--- application/config/googleplus.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$config['googleplus']['application_name'] = 'wabot';
-$config['googleplus']['client_id']        = '';
-$config['googleplus']['client_secret']    = '';
-$config['googleplus']['redirect_uri']     = 'http://localhost/chatwabot/doLogin';
-$config['googleplus']['api_key']          = '';
-$config['googleplus']['scopes']           = array();
-?>
