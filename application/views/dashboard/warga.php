e156080d2ec1ea15266bf529eb1b007005a03760
hudamiftakh <hudamiftakh8@gmail.com>
2025-05-18 21:13:09 +0700

    perbaikan

--- application/views/dashboard/warga.php
+++ application/views/dashboard/warga.php
@@ -7,7 +7,7 @@ $this->load->library('encryption');
     <div class="card-body px-4 py-3">
         <div class="row align-items-center">
             <div class="col-9">
-                <h4 class="fw-semibold mb-8">Warga Managements</h4>
+                <h4 class="fw-semibold mb-8">Warga</h4>
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item">
