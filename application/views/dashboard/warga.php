f2eb1df7aaf95429ee5aaaa8ee365e92eaf996a2
hudamiftakh <hudamiftakh8@gmail.com>
2025-05-18 21:14:05 +0700

    perbaikan data

--- application/views/dashboard/warga.php
+++ application/views/dashboard/warga.php
@@ -7,14 +7,14 @@ $this->load->library('encryption');
     <div class="card-body px-4 py-3">
         <div class="row align-items-center">
             <div class="col-9">
-                <h4 class="fw-semibold mb-8">Warga</h4>
+                <h4 class="fw-semibold mb-8">Manajemen Warga</h4>
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item">
                             <a class="text-muted text-decoration-none" href="./">Dashboard</a>
                         </li>
                         <li class="breadcrumb-item" aria-current="page">Warga</li>
-                        <li class="breadcrumb-item" aria-current="page">Warga Management</li>
+                        <li class="breadcrumb-item" aria-current="page">Manajemen Warga</li>
                     </ol>
                 </nav>
             </div>
