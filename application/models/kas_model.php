5fc177d053500ee1238430c73dea075f32bdf174
hudamiftakh <hudamiftakh8@gmail.com>
2025-08-10 15:39:00 +0700

    perbaikan

--- application/models/kas_model.php
+++ application/models/kas_model.php
@@ -1,6 +1,6 @@
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
-                        
+                            
 class Kas_model extends CI_Model 
 {
     function __construct()
